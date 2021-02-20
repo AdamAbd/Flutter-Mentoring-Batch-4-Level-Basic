ec5b58be186135e49f886ec560e5ace52d74a338
Adam Abdurrahman <Adam2802002@gmail.com>
2021-02-20 08:57:06 +0700

    first change

---
+++ lib/Screens/Pages/pages.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_mentoring_basic_batch_4/Screens/Widgets/widgets.dart';
+import 'package:flutter_mentoring_basic_batch_4/Shared/shared.dart';
+
+part 'sign_up_page.dart';
+part 'create_post_page.dart';
+part 'your_blogs_page.dart';
+part 'payment_card_cupertino.dart';
+part 'convertion_page.dart';
